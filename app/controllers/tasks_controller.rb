49964451991355813d4f526e9f9b896ac0b4143a
ren.wakamaki <wakamakiren@icloud.com>
2021-07-25 03:06:39 +0000

    modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -28,15 +30,29 @@ class TasksController < ApplicationController
   end
 
   def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
-  
+
   private
 
-  # Strong Parameter
-  def message_params
-    params.require(:message).permit(:content)
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content, :title)
   end
 end
