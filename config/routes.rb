f27dae2f418b179a7bc48d135106c21ecd7a337c
ren.wakamaki <wakamakiren@icloud.com>
2021-07-30 06:16:49 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
+  
   get 'signup', to: 'tasks#index'
   resources :users, only: [:index, :show, :create]
   resources :tasks
