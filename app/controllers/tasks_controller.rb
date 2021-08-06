31b305f8c14b35ac6691e676f622791575b90c8d
ren.wakamaki <wakamakiren@icloud.com>
2021-08-06 02:58:35 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,13 +3,15 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
 
   def index
-    @pagy, @tasks = pagy(Task.all)
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
   end
 
   def show
     @task = Task.find(params[:id])
   end
-
   def new
     @task = Task.new
   end
