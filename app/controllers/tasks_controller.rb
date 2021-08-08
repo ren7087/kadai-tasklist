7882e3ef0f7cc83be807d0d5128ef9c81f60af37
ren.wakamaki <wakamakiren@icloud.com>
2021-08-08 13:03:54 +0000

    add pagination

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @tasks = current_user.tasks
@@ -8,6 +7,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = current_user.tasks.find(params[:id])
   end
   
   def new
@@ -27,9 +27,11 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
+    @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -40,6 +42,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = current_user.tasks.find(params[:id])
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
@@ -51,10 +54,4 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-  def correct_user
-    @task = current_user.tasks.find_by(id: params[:id])
-    unless @task
-      redirect_to root_url
-    end
-  end
 end
