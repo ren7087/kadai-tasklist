1431e0a7841aad06971e446ef0a3044564ec3877
ren.wakamaki <wakamakiren@icloud.com>
2021-08-09 05:40:51 +0000

    tasklist1222

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :set_user, only: [:show]
+ 
 
   def index
-    @users = User.all
   end
 
   def show
@@ -15,7 +14,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to root_url
@@ -33,8 +31,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
-  def set_user
-      @user = User.find(prams[:id])
-  end
+  
     
 end
